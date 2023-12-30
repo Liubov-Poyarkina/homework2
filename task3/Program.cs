6861ee3530cfdde09877a58b885e43b7cf809d42
Vergilia <poyarkina.l.i@yandex.ru>
2023-12-30 19:05:23 +0300

    Add task3

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+
+int num = new Random().Next(10, 100);
+
+int FirstNumber = num / 10;
+
+int SecondNumber = num % 10;
+
+Console.WriteLine(num);
+Console.WriteLine(FirstNumber);
+Console.WriteLine(SecondNumber);
+
+if(FirstNumber > SecondNumber)
+{
+    Console.WriteLine($"Число {FirstNumber} больше");
+}
+else
+{
+    Console.WriteLine($"Число {SecondNumber} больше");
+}
