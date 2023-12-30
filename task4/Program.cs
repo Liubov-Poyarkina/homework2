b48616e608a5b8655062c160e8ea5b555ce69711
Vergilia <poyarkina.l.i@yandex.ru>
2023-12-30 20:06:18 +0300

    Add Task 4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, 
+// а на выходе показывает его цифры через запятую.
+
+
+Console.WriteLine("Введите число:");
+string num = Convert.ToString(Console.ReadLine());
+
+ for (int i = 0; i < num.Length; i++)
+{
+    Console.Write($"{num[i]}, ");
+}
+Console.Write("\b\b ");
