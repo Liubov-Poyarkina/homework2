80770511cb27012283bde18479070b88e00fdf9b
Vergilia <poyarkina.l.i@yandex.ru>
2023-12-30 18:11:33 +0300

    Add task1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите числo:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine($"Число {a} кратно одновременно числам 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {a} не кратно одновременно 7 и 23");
+}
